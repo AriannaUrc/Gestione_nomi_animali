6621341ef48b43ac773fe881aea72a2327c50be8
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-02-01 13:13:50 +0100

    Aggiungere i file di progetto.

---
+++ Gestione_funzione/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestione_funzione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
