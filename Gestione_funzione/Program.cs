bc3090a6f42c2e83637ce815ec0aff401ef3e23b
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-02-01 13:42:55 +0100

    add funzione Ricerca

--- Gestione_funzione/Program.cs
+++ Gestione_funzione/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//0 uscita dal menu
-//1 Aggiunta nome animale
+//0 uscita dal menu ok.
+//1 Aggiunta nome animale ok.
 //2 Cancella primo nome trovato animale
 //3 Ordinamento in ordine alfabetico (bubbleSort)
 //4 Ricerca sequenziale animali
 //5 Visualizza animali ripetuti (anche quante volte sono ripetuti)
 //6 Modifica nome
-//7 Visualizzazione tutti gli animali
+//7 Visualizzazione tutti gli animali ok.
 //8 Ricerca nomi più lunghi e più corti + visualizzazione
 //9 Cancellazione di tutti i nomi ripetuti
 
@@ -35,10 +35,26 @@ namespace Gestione_funzione
             }
         }
 
+        static int Ricerca(string[] nomi, ref int lenght, string ricerca)
+        {
+            int pos=-1;
+            for (int i = 0; i < lenght; i++)
+            {
+                if (nomi[i] == ricerca)
+                {
+                    pos = i;
+                    break;
+                }
+            }
+
+            return pos;
+        }
+
         static void Main(string[] args)
         {
             string[] nomi = new string[100];
-            int scelta, lenght=0;
+            string ricerca;
+            int scelta, lenght=0, pos;
             bool continua=true;
             
 
@@ -64,6 +80,17 @@ namespace Gestione_funzione
                         break;
 
                     case 4:
+                        Console.WriteLine("Inserire elemento da ricercare");
+                        ricerca = Console.ReadLine();
+                        pos=Ricerca(nomi, ref lenght, ricerca);
+                        if (pos == -1)
+                        {
+                            Console.WriteLine("Elemento non trovato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("L'indice dell'elemento è: "+pos);
+                        }
                         break;
 
                     case 5:
